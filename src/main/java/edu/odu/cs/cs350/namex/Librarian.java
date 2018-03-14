c5bc11c4c18ccd1ed76d6a39ca79c48de4ae768a
rsandor <rsand006@odu.edu>
2018-03-14 19:28:14 -0400

    Added personal name class.

--- src/main/java/edu/odu/cs/cs350/namex/Librarian.java
+++ src/main/java/edu/odu/cs/cs350/namex/Librarian.java
@@ -1,7 +1,7 @@
 package edu.odu.cs.cs350.namex;
 /**
  * 
- * @author Raphael J. Sandor
+ *
  * The Librarian class should handle the input of the 
  * program this entails both type of users (CLI, and API).
  *
