72e74f28525b945d37f96eee79a657a93d141c91
JosephIsMee <jrugh001@odu.edu>
2018-03-14 19:13:50 -0400

    Cleaned up files for src/test.

--- src/main/java/edu/odu/cs/cs350/namex/Librarian.java
+++ src/main/java/edu/odu/cs/cs350/namex/Librarian.java
@@ -1,15 +1,5 @@
 package edu.odu.cs.cs350.namex;
-/**
- * 
- * @author Raphael J. Sandor
- * The Librarian class should handle the input of the 
- * program this entails both type of users (CLI, and API).
- *
- */
-
 
 public class Librarian {
-
-	
 	
 }
