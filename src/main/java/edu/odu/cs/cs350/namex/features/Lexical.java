5d875ac6467856a145480b8d48d957ce69765e2f
JosephIsMee <jrugh001@odu.edu>
2018-03-14 19:24:32 -0400

    Created the basis for features.

---
+++ src/main/java/edu/odu/cs/cs350/namex/features/Lexical.java
@@ -0,0 +1,8 @@
+package edu.odu.cs.cs350.namex.features;
+
+public class Lexical implements Feature {
+
+	@Override
+	public void doesApply(String word) {
+	}
+}
---
+++ src/main/java/edu/odu/cs/cs350/namex/features/PartsOfSpeech.java
@@ -0,0 +1,8 @@
+package edu.odu.cs.cs350.namex.features;
+
+public class PartsOfSpeech implements Feature {
+
+	@Override
+	public void doesApply(String word) {
+	}
+}
