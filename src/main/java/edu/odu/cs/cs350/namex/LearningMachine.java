dde9dbf4f6589fa6cef44cffb4908eb92ded67e2
ctremble <ctrem001@odu.edu>
2018-03-14 21:06:56 -0400

    Added stubs for LearningMachine - Unfinished

--- src/main/java/edu/odu/cs/cs350/namex/LearningMachine.java
+++ src/main/java/edu/odu/cs/cs350/namex/LearningMachine.java
@@ -2,4 +2,26 @@ package edu.odu.cs.cs350.namex;
 
 public class LearningMachine {
 
+	
+	public char calculateWord(String word) {
+		char test = 0;
+		return test;
+	}
+	public void learn(char blockText) {
+		
+	}
+	
+	public char judgeBlock(char inputedBlock) {
+		return inputedBlock; 
+	}
+	
+	public void addCharToList(char resultOfCalc) {
+		
+	}
+	// This is simply an interface to add a character to collection of results 
+	// for a given block.
+	
+	public void readFeatureList() {
+		
+	}
 }
