651466f1e2703f4b3e776eb53f16c255bb8f4812
JosephIsMee <jrugh001@odu.edu>
2018-03-14 20:17:20 -0400

    Added stubbed methods to feature classes.

--- src/main/java/edu/odu/cs/cs350/namex/features/Gazetteer.java
+++ src/main/java/edu/odu/cs/cs350/namex/features/Gazetteer.java
@@ -3,6 +3,42 @@ package edu.odu.cs.cs350.namex.features;
 public class Gazetteer implements Feature {
 	
 	@Override
-	public void doesApply(String word) {	
+	public String doesApply(String word) {	
+		return "";
+	}
+	
+	public void performDatabaseFetching() {
+	}
+	
+	private boolean isCommonFirstName(String word) {
+		return false;
+	}
+	
+	private boolean isCommonLastName(String word) {
+		return false;
+	}
+	
+	private boolean isDictionaryWord(String word) {
+		return false;
+	}
+	
+	private boolean hasSuffixes(String word) {
+		return false;
+	}
+	
+	private boolean isUsaCity(String word) {
+		return false;
+	}
+	
+	private boolean isUsaState(String word) {
+		return false;
+	}
+	
+	private boolean isTerritory(String word) {
+		return false;
+	}
+	
+	private boolean isCountry(String word) {
+		return false;
 	}
 }
--- src/main/java/edu/odu/cs/cs350/namex/features/Lexical.java
+++ src/main/java/edu/odu/cs/cs350/namex/features/Lexical.java
@@ -3,6 +3,23 @@ package edu.odu.cs.cs350.namex.features;
 public class Lexical implements Feature {
     
 	@Override
-	public void doesApply(String word) {
+	public String doesApply(String word) {
+		return "";
+	}
+	
+	private boolean hasNumber(String word) {
+		return false;
+	}
+	
+	private boolean isCapitalized(String word) {
+		return false;
+	}
+	
+	private boolean isNewLine(String word) {
+		return false;
+	}
+	
+	private boolean isWhitespace(String word) {
+		return false;
 	}
 }
