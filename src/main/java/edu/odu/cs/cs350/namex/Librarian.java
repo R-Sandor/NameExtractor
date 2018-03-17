009e95e65ae6c6aef0abc5ea03b9494619e003e5
rsandor <rsand006@odu.edu>
2018-03-17 09:21:43 -0400

    Completed the tests for the CLI and API interfaces on the Librarian.

--- src/main/java/edu/odu/cs/cs350/namex/Librarian.java
+++ src/main/java/edu/odu/cs/cs350/namex/Librarian.java
@@ -13,23 +13,36 @@ import java.util.ArrayList;
 
 public class Librarian {
 	
-	private ArrayList<String> outBlock; 
+	private ArrayList<String> outBlock;
 	
+	/*
+	 * Read text files from the CLI interface.
+	 */
 	public void readInput(File file) {
 		
 	}
 	
-	public void readInput(String text) {
+	/*
+	 * Read text from the API.
+	 */
+	public void readInput(String text){
 		
 	}
 	
-	public ArrayList<String> getBlocks()
-	{
+	public ArrayList<String> getBlocks(){
 		return null;
 	}
 	
-	public int numOfBlocks()
-	{
+	
+	public int numOfBlocks(){
 		return 0;
+	}
+	
+	/*
+	 * The processing of the blocks is calling each block 
+	 * to the PNE system.
+	 */
+	public void processBlocks(){
+		
 	}
 }
--- src/test/edu/odu/cs/cs350/namex/TestLibrarian.java
+++ src/test/edu/odu/cs/cs350/namex/TestLibrarian.java
@@ -1,9 +1,16 @@
 package edu.odu.cs.cs350.namex;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Test;
 
@@ -11,13 +18,12 @@ public class TestLibrarian {
 	
 	
 	@Test
-	public void testReadApi()
-	{
+	public void testReadApi(){
 		Librarian librarian = new Librarian();
 		String exampleInput = "Name Extraction -- Requirements Definition\nSteven J Zeil\nJan 20, 2016";
 		librarian.readInput(exampleInput);
-		ArrayList block = librarian.getBlocks();
-		assert(block.contains(exampleInput));
+		ArrayList<String> block = librarian.getBlocks();
+		assertTrue(block.contains(exampleInput));
 		
 		assertEquals(1, librarian.getBlocks());
 		
@@ -29,14 +35,41 @@ public class TestLibrarian {
 	 */
 	@Test
 	public void testReadCLI() {
-		
-		File file = new File("test1.txt");
+		int openNerCount = 0;
+		int nerCloseCount = 0;
+		List<String> textBlocks = new ArrayList<>();
+		List<String> nerTestLines = new ArrayList<>();
+
+		File file = new File("src/main/resources/TestNER.txt");
 		Librarian librarian = new Librarian();
-		
 		librarian.readInput(file);
+		try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
+			bufferedReader.lines().forEach(nerTestLines::add);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		for (String line : nerTestLines) {
+			
+			if (line.contains("<NER>")) {
+				openNerCount++;
+				textBlocks.add(line);
+			}
+			if (line.contains("</NER>"))
+				nerCloseCount++;
+		}
+		
+		assertEquals(openNerCount, nerCloseCount);
+		
+		assertEquals(openNerCount, librarian.getBlocks());
+		
+		assertEquals(textBlocks.get(1), librarian.getBlocks().get(1));
+		
+		
 		
 		
 	}
 
+	
+	
 	
 }
