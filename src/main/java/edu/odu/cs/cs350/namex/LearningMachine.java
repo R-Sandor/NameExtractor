7915d56c2de0babe800b095694f44eb11a45a2ce
jrugh <jrugh001@odu.edu>
2018-03-18 19:14:44 -0400

    Provided a list to map text to features.

--- src/main/java/edu/odu/cs/cs350/namex/LearningMachine.java
+++ src/main/java/edu/odu/cs/cs350/namex/LearningMachine.java
@@ -1,21 +1,26 @@
 package edu.odu.cs.cs350.namex;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import edu.odu.cs.cs350.namex.features.Lexical;
 
 public class LearningMachine {
 
-	
 	public char calculateWord(String text) {
 		char test = 0;
 		return test;
 	}
+	
 	public void learn(String blockText) {
+		
 		Lexical lexical = new Lexical();
 		List<String> splitText = lexical.separateText(blockText);
-		for (String s : splitText) {
-			//mappedStrings.put(s," ");
+		List<String> mappedFeatures = new ArrayList<>();
+		
+		// Provide a mapping between the 'splitText' and the 'mappedFeatures' lists
+		for (String text :splitText) {
+			
 		}
 		
 	}
