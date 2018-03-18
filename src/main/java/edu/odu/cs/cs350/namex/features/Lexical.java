e57d993cac2588da17cbf53ff470a0586d57ae75
shayne <shayn005@odu.edu>
2018-03-17 20:43:27 -0400

    Partial Start to Lexical Class

--- src/main/java/edu/odu/cs/cs350/namex/features/Lexical.java
+++ src/main/java/edu/odu/cs/cs350/namex/features/Lexical.java
@@ -4,6 +4,7 @@ public class Lexical implements Feature {
     
 	@Override
 	public String doesApply(String text) {
+		isPuncutation(text);//needs more
 		return "";
 	}
 	
@@ -11,11 +12,38 @@ public class Lexical implements Feature {
 		return false;
 	}
 	
-	private boolean isCapitalized(String text) {
+	private boolean isCapitalized(String text) {        
+		    for(int i=text.length()-1; i>=0; i--) {
+		        if(Character.isUpperCase(text.charAt(i))) {
+		            return true;
+		        }
+		    }
 		return false;
 	}
 	
 	private boolean isNewLine(String text) {
+		return false;
+	}
+	
+	private boolean isAllCaps(String text) {
+		return false;
+	}
+	
+	private boolean isCapitalLetter(String text) {
+	    for(int i=text.length()-1; i>=0; i--) {
+	        if(Character.isUpperCase(text.charAt(i))) {
+	            return true;
+	        }
+	    }
+	return false;
+}
+	
+	private boolean isPuncutation(String text) {
+		String letter[] = {"a", "b", "."};
+		String punctutations = ".,;:?!$%'" ;
+		if (punctutations.contains(letter[a])) {
+			return true;
+		}
 		return false;
 	}
 	
