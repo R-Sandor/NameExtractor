3df7b26bc6a19bf2f56556d130747da30b2e3144
shayne <shayn005@odu.edu>
2018-03-18 23:21:26 -0400

    Personal Name Extractor Train Learning Machine test, requires a resource file and work on assertEquals line

---
+++ src/test/edu/odu/cs/cs350/namex/TestPersonalNameExtractor.java
@@ -0,0 +1,44 @@
+package edu.odu.cs.cs350.namex;
+
+import java.util.List;
+
+import org.junit.Test;
+
+public class TestPersonalNameExtractor {
+	@Test
+	public void TestTrainLearningMachine() {
+		
+		PersonalNameExtractor PersonalNameExtractor = new PersonalNameExtractor();
+		
+		List<String> learningTestLines = TestUtil.readLinesFromFile(" ");
+		
+		boolean testLearn = false;
+		StringBuilder blockOfTextBuilder = new StringBuilder("");
+		String blockOfText = "", learnOutput = "";
+		for (int i = 0; i < learningTestLines.size(); i++) {
+			
+			String line = learningTestLines.get(i);
+			
+			// It will now append the line onto the block of text.
+			if (!testLearn) {
+				blockOfTextBuilder.append(line);
+				if (line.contains("--END--")) {
+					blockOfText = blockOfTextBuilder.toString();
+					blockOfTextBuilder = new StringBuilder("");
+					i++; // Skip number line.
+					learnOutput = learningTestLines.get(i);
+					testLearn = true;
+				} else {
+					blockOfTextBuilder.append("\n");
+				}
+			}
+			
+			if (testLearn) {
+				String testBlock = blockOfText.substring(0, blockOfText.length() - 7);
+				//assertEquals(learnOutput, PersonalNameExtractor.trainLearningMachine(testBlock));
+				testLearn = false;
+			}
+		}
+		
+	}
+}
