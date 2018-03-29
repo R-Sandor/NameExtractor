03bca82681e2b6cdc6641bbcdf0f67b9c85b3cde
rsandor <rsand006@odu.edu>
2018-03-29 14:42:29 -0400

    working on input

--- src/main/java/edu/odu/cs/cs350/namex/Main.java
+++ src/main/java/edu/odu/cs/cs350/namex/Main.java
@@ -1,11 +1,15 @@
 package edu.odu.cs.cs350.namex;
 
+import java.io.Console;
+
 public class Main {
 
 	// Used to interface with the CLI.
 	public static void main(String args[]) throws Exception {
 		Librarian librarian = new Librarian();
 
+	
+		
 		if (args.length > 0)
 		{
 			for (int i=0; i< args.length;i++)
