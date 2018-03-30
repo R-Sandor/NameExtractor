4d4e424d92b2680c2d0389636f9695e2574cf12c
rsandor <rsand006@odu.edu>
2018-03-30 16:22:23 -0400

    Better every time.

--- src/main/resources/deploy.sh
+++ src/main/resources/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-rsync -auvz -e "ssh" /build/docs/ rsandor@atria.cs.odu.edu:./
-rsync -auvz -e "ssh" /reports/ rsandor@atria.cs.odu.edu:./
+rsync -auvz -e "ssh" ./build/docs/ rsandor@atria.cs.odu.edu:./
+rsync -auvz -e "ssh" .build/reports/ rsandor@atria.cs.odu.edu:./
