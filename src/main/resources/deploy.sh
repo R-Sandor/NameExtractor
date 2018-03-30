bf24fbfa2db01dc5714baeee31e954d1812bdb81
rsandor <rsand006@odu.edu>
2018-03-30 16:39:41 -0400

    now?

--- src/main/resources/deploy.sh
+++ src/main/resources/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd
+cd build
 pwd
-rsync -auvz -e "ssh" ./build/docs/ rsandor@atria.cs.odu.edu:./
-rsync -auvz -e "ssh" .build/reports/ rsandor@atria.cs.odu.edu:./
+rsync -auvz -e "ssh" build/docs/ rsandor@atria.cs.odu.edu:./
+rsync -auvz -e "ssh" build/reports/ rsandor@atria.cs.odu.edu:./
